73ce19067aab00876419840d5dc692bea06c1df4
kenny.tran <tranngocquan@gmail.com>
2025-11-30 15:42:18 +0700

    Chore: Standardize build configuration and enforce code style

--- src/LegacyOrderService/Common/Result.cs
+++ src/LegacyOrderService/Common/Result.cs
@@ -19,6 +19,6 @@ public class Result<T>
         ErrorMessage = error;
     }
 
-    public static Result<T> Success(T value) => new Result<T>(true, value, null);
-    public static Result<T> Failure(string error) => new Result<T>(false, default, error);
+    public static Result<T> Success(T value) => new Result<T>(true, value, null!);
+    public static Result<T> Failure(string error) => new Result<T>(false, default!, error);
 }
