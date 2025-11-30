37126f44e849615f63e974c7862f880e32364b63
kenny.tran <tranngocquan@gmail.com>
2025-11-30 15:16:09 +0700

    Enhance logging, config, and Docker support

--- src/LegacyOrderService/Utils/ConsoleHelper.cs
+++ src/LegacyOrderService/Utils/ConsoleHelper.cs
@@ -55,4 +55,24 @@ public static class ConsoleHelper
         Console.WriteLine(message);
         Console.ResetColor();
     }
+
+    public static void PrintWelcome()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("""
+         _                                     ____          _              _____                 _          
+        | |                                   / __ \        | |            / ____|               (_)         
+        | |     ___  __ _  __ _  ___ _   _   | |  | |_ __ __| | ___ _ __  | (___   ___ _ ____   ___  ___ ___ 
+        | |    / _ \/ _` |/ _` |/ __| | | |  | |  | | '__/ _` |/ _ \ '__|  \___ \ / _ \ '__\ \ / / |/ __/ _ \
+        | |___|  __/ (_| | (_| | (__| |_| |  | |__| | | | (_| |  __/ |     ____) |  __/ |   \ V /| | (_|  __/
+        |______\___|\__, |\__,_|\___|\__, |   \____/|_|  \__,_|\___|_|    |_____/ \___|_|    \_/ |_|\___\___|
+                     __/ |            __/ |                                                                  
+                    |___/            |___/                                                                           
+        """);
+        Console.WriteLine("-----------------------------------------");
+        Console.WriteLine("     Welcome to Order Processor!      ");
+        Console.WriteLine("-----------------------------------------");
+        Console.ResetColor();
+        Console.WriteLine(); 
+    }
 }
